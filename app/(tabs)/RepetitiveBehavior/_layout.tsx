d1548552458f0cd3a2701bd6e8cde124377939e1
Yasanjith Rajapathirane <yasanjithrajapathirane@gmail.com>
2025-03-20 08:21:15 +0530

    final ui changes

--- app/(tabs)/RepetitiveBehavior/_layout.tsx
+++ app/(tabs)/RepetitiveBehavior/_layout.tsx
@@ -1,22 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { Tabs } from "expo-router";
+// import { StyleSheet, Text, View } from "react-native";
+// import React from "react";
+// import { Tabs } from "expo-router";
 
-const SocialRepLayout = () => {
-  return (
-    <>
-      <Tabs>
-        <Tabs.Screen name="index" options={{ title: "Customize your own story", headerShown: false }} />
-        <Tabs.Screen name="game" options={{ title: "Lets get started!", headerShown: false }} />
-        <Tabs.Screen name="Scorecard" options={{ title: "Progress", headerShown: false }} />
+// const SocialRepLayout = () => {
+//   return (
+//     <>
+//       <Tabs>
+//         <Tabs.Screen name="index" options={{ title: "Customize your own story", headerShown: false }} />
+//         <Tabs.Screen name="game" options={{ title: "Lets get started!", headerShown: false }} />
+//         <Tabs.Screen name="Scorecard" options={{ title: "Progress", headerShown: false }} />
 
        
-        {/* <Tabs.Screen name="game" options={{ title: "Game", headerShown: false }} /> */}
-      </Tabs>
-    </>
-  );
-};
+//         {/* <Tabs.Screen name="game" options={{ title: "Game", headerShown: false }} /> */}
+//       </Tabs>
+//     </>
+//   );
+// };
 
-export default SocialRepLayout;
+// export default SocialRepLayout;
 
-const styles = StyleSheet.create({});
+// const styles = StyleSheet.create({});
